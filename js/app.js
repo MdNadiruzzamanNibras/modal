6d9886e024e2b0b91d7027f7035daea48fef3061
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-03-14 10:41:50 +0600

    save

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 document.getElementById('open').addEventListener('click', ()=>{
-    console.log('click')
+    
     document.getElementById('modal-container').classList.add('show')
 })
 document.getElementById('close').addEventListener('click', ()=>{
